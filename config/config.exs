482e8cec316560de163a25b4d8d39fbe7607b23c
Andrew Forward <aforward@gmail.com>
2019-04-12 16:56:25 -0400

    $ mix phx.new hangman --no-ecto
    The directory /tmp/hangman already exists. Are you sure you want to continue? [Yn] Y
    * creating hangman/config/config.exs
    * creating hangman/config/dev.exs
    * creating hangman/config/prod.exs
    * creating hangman/config/prod.secret.exs
    * creating hangman/config/test.exs
    * creating hangman/lib/hangman/application.ex
    * creating hangman/lib/hangman.ex
    * creating hangman/lib/hangman_web/channels/user_socket.ex
    * creating hangman/lib/hangman_web/views/error_helpers.ex
    * creating hangman/lib/hangman_web/views/error_view.ex
    * creating hangman/lib/hangman_web/endpoint.ex
    * creating hangman/lib/hangman_web/router.ex
    * creating hangman/lib/hangman_web.ex
    * creating hangman/mix.exs
    * creating hangman/README.md
    hangman/README.md already exists, overwrite? [Yn] Y
    * creating hangman/.formatter.exs
    * creating hangman/.gitignore
    hangman/.gitignore already exists, overwrite? [Yn] Y
    * creating hangman/test/support/channel_case.ex
    * creating hangman/test/support/conn_case.ex
    * creating hangman/test/test_helper.exs
    * creating hangman/test/hangman_web/views/error_view_test.exs
    * creating hangman/lib/hangman_web/gettext.ex
    * creating hangman/priv/gettext/en/LC_MESSAGES/errors.po
    * creating hangman/priv/gettext/errors.pot
    * creating hangman/lib/hangman_web/controllers/page_controller.ex
    * creating hangman/lib/hangman_web/templates/layout/app.html.eex
    * creating hangman/lib/hangman_web/templates/page/index.html.eex
    * creating hangman/lib/hangman_web/views/layout_view.ex
    * creating hangman/lib/hangman_web/views/page_view.ex
    * creating hangman/test/hangman_web/controllers/page_controller_test.exs
    * creating hangman/test/hangman_web/views/layout_view_test.exs
    * creating hangman/test/hangman_web/views/page_view_test.exs
    * creating hangman/assets/webpack.config.js
    * creating hangman/assets/.babelrc
    * creating hangman/assets/css/app.css
    * creating hangman/assets/css/phoenix.css
    * creating hangman/assets/js/app.js
    * creating hangman/assets/js/socket.js
    * creating hangman/assets/package.json
    * creating hangman/assets/static/robots.txt
    * creating hangman/assets/static/images/phoenix.png
    * creating hangman/assets/static/favicon.ico
    
    Fetch and install dependencies? [Yn] Y
    * running mix deps.get
    * running mix deps.compile
    * running cd assets && npm install && node node_modules/webpack/bin/webpack.js --mode development
    
    We are almost there! The following steps are missing:
    
        $ cd hangman
    
    Start your Phoenix app with:
    
        $ mix phx.server
    
    You can also run your app inside IEx (Interactive Elixir) as:
    
        $ iex -S mix phx.server

---
+++ config/config.exs
@@ -0,0 +1,27 @@
+# This file is responsible for configuring your application
+# and its dependencies with the aid of the Mix.Config module.
+#
+# This configuration file is loaded before any dependency and
+# is restricted to this project.
+
+# General application configuration
+use Mix.Config
+
+# Configures the endpoint
+config :hangman, HangmanWeb.Endpoint,
+  url: [host: "localhost"],
+  secret_key_base: "JF7FYPQtBKHNeakLe1xES6jB5oLYmBk6EkLm5nbUq9JXlAS8licCxSJz0SK1pxBU",
+  render_errors: [view: HangmanWeb.ErrorView, accepts: ~w(html json)],
+  pubsub: [name: Hangman.PubSub, adapter: Phoenix.PubSub.PG2]
+
+# Configures Elixir's Logger
+config :logger, :console,
+  format: "$time $metadata[$level] $message\n",
+  metadata: [:request_id]
+
+# Use Jason for JSON parsing in Phoenix
+config :phoenix, :json_library, Jason
+
+# Import environment specific config. This must remain at the bottom
+# of this file so it overrides the configuration defined above.
+import_config "#{Mix.env()}.exs"
