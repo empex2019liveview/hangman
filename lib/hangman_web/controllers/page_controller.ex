dfce35f1e43a522c5c489686b651167e1041359a
Andrew Forward <aforward@gmail.com>
2019-04-12 18:20:50 -0400

    Move controller to liveview

--- lib/hangman_web/controllers/page_controller.ex
+++ lib/hangman_web/controllers/page_controller.ex
@@ -1,52 +1,8 @@
 defmodule HangmanWeb.PageController do
   use HangmanWeb, :controller
-  alias Diet.Stepper
+  alias Phoenix.LiveView.Controller, as: LiveController
 
   def index(conn, _params) do
-    s = Stepper.new(Hangman, "shawshank")
-    {_r, s} = Stepper.run(s, {:make_move, "a"})
-    {_r, s} = Stepper.run(s, {:make_move, "z"})
-    {_r, s} = Stepper.run(s, {:make_move, "w"})
-    {_r, s} = Stepper.run(s, {:make_move, "o"})
-    {_r, s} = Stepper.run(s, {:make_move, "x"})
-    {r, s} = Stepper.run(s, {:make_move, "y"})
-
-    conn
-    |> assign_game(s)
-    |> assign_reply("y", r)
-    |> render("index.html")
-  end
-
-  defp assign_game(conn), do: assign_game(conn, Stepper.new(Hangman, "shawshank"))
-
-  defp assign_game(conn, stepper) do
-    conn
-    |> assign(:stepper, stepper)
-    |> assign(:rope, body_part(6, stepper))
-    |> assign(:head, body_part(5, stepper))
-    |> assign(:body, body_part(4, stepper))
-    |> assign(:left_arm, body_part(3, stepper))
-    |> assign(:right_arm, body_part(2, stepper))
-    |> assign(:left_leg, body_part(1, stepper))
-    |> assign(:right_leg, body_part(0, stepper))
-    |> assign(:answer, stepper.model.word_to_guess |> Enum.join())
-    |> assign(:turns_left, stepper.model.turns_left)
-    |> assign(:word_so_far, Hangman.Model.word_so_far(stepper.model))
-    |> assign(:move_reply, nil)
-    |> assign(:letter, nil)
-  end
-
-  defp assign_reply(socket, letter, reply) do
-    socket
-    |> assign(:move_reply, reply)
-    |> assign(:letter, letter)
-  end
-
-  defp body_part(num, s) do
-    cond do
-      s.model.turns_left == num -> :new
-      s.model.turns_left < num -> :on
-      s.model.turns_left > num -> :off
-    end
+    LiveController.live_render(conn, HangmanWeb.PageLiveView, session: %{})
   end
 end
--- lib/hangman_web/views/page_view.ex
+++ lib/hangman_web/views/page_view.ex
@@ -1,26 +1,3 @@
 defmodule HangmanWeb.PageView do
   use HangmanWeb, :view
-
-  @colours [
-    "#5ED0FA",
-    "#54D1DB",
-    "#A368FC",
-    "#F86A6A",
-    "#FADB5F",
-    "#65D6AD",
-    "#127FBF",
-    "#0E7C86",
-    "#690CB0",
-    "#AB091E",
-    "#CB6E17",
-    "#147D64"
-  ]
-
-  @hit "#D64545"
-
-  def colour(), do: @colours |> Enum.random()
-
-  def person(:on), do: @colours |> Enum.random()
-  def person(:off), do: "none"
-  def person(:new), do: @hit
 end
